65bee60004510e603d38234783dc2c3fd0418984
cycle-path <tatendazimba@gmail.com>
2018-07-06 11:51:12 +0200

    Added buttons wireframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { TextComponent } from './text.component';
+import { ButtonsComponent } from './buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    PageNotFoundComponent,
+    ButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+          path: 'text',
+          component: TextComponent
+      },
+      {
+        path: 'buttons',
+        component: ButtonsComponent
+      },
+      {
+        path: '',
+        redirectTo: '/text',
+        pathMatch: 'full'
+      },
+      {
+          path: '**',
+          component: PageNotFoundComponent
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
